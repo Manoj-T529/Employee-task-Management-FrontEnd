99b5c20b5aaf45d0540d3f5327aa0570869e9ce1
Manoj Thangamani <manoj.thangamani@psiog.com>
2026-04-10 14:15:15 +0530

    final changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  apiBaseUrl:  'http://taskforge.space/api/v1'
+  apiBaseUrl:  'https://taskforge.space/api/v1'
 };
 
 // 'http://mini-jira-lb-ec2-1498747831.us-east-2.elb.amazonaws.com/api/v1'
